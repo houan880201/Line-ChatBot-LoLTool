51267766c8a74c3db480edc42da1aefbf771a7f4
Howard <houan.howard@gmail.com>
2019-07-22 18:13:54 +0800

    remove unidecode

--- app.py
+++ app.py
@@ -141,11 +141,16 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 	qryChamp = event.message.text
-	res = valid_champ(qryChamp)
-	message = TextSendMessage(qryChamp)
-	line_bot_api.reply_message(event.reply_token, message)
-	msg = TextSendMessage(str(res))
+	msg = "before"
+	try:
+		msg = "nothing"
+		res = valid_champ(qryChamp)
+	except:
+		msg = "error"
+	message = TextSendMessage(msg)
 	line_bot_api.reply_message(event.reply_token, msg)
+	#msg = TextSendMessage(str(res))
+	#line_bot_api.reply_message(event.reply_token, msg)
 
 	'''
 	if valid_champ(qryChamp):
