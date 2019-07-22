fedcddcdd9ce5b17ea9788c8f8f5580bc6db6c21
Howard <houan.howard@gmail.com>
2019-07-22 17:36:15 +0800

    Try

--- app.py
+++ app.py
@@ -20,107 +20,6 @@ line_bot_api = LineBotApi('JeJ+t2/bwCVSFVyBTdWPBO8VVeY826+LV3W/S/71XUygxI+4Epp8O
 # Channel Secret
 handler = WebhookHandler('88095fca9a435628a8522c92f8d601e9')
 
-counter_cols = ['1st Counter', '2nd Counter', '3rd Counter', '4th Counter', '5th Counter', '6th Counter']
-against_cols = ['1st Strong Against', '2nd Strong Against', '3rd Strong Against', '4th Strong Against', '5th Strong Against', '6th Strong Against']
-partner_cols = ['1st Good Partner', '2nd Good Partner', '3rd Good Partner', '4th Good Partner', '5th Good Partner', '6th Good Partner']
-tips_cols = ['Counter Tip One','Counter Tip Two','Counter Tip Three','Counter Tip Four']
-
-def valid_champ(name):
-    champs = get_all_champions()
-    name
-    if name in champs or name.capitalize() in champs:
-        return True
-    else:
-        return False
-
-def get_all_champions():
-    # JSON is hosted at myjson.com as well just in case
-    # url = 'https://api.myjson.com/bins/tkg0v'
-    with open('champs.json') as json_data:
-        data = json.load(json_data)
-        allChampions = [unidecode.unidecode(listing['Champion Names']) for listing in data]
-        return allChampions
-
-def parse_name(url):
-    return url.split('/')[-1]
-
-def get_loc_names(counter_cols):
-    loc_names = []
-    for col in counter_cols:
-        loc_names.append(col + " Location")
-    return loc_names
-
-def get_counter(name):
-    with open('champs.json') as json_data:
-        data = json.load(json_data)
-        counters = []
-        locs = []
-        for champs in data:
-            if champs['Champion Names'] == name:
-                for col in counter_cols:
-                    counters.append(parse_name(champs[col]))
-                for col in get_loc_names(counter_cols):
-                    locs.append(champs[col])
-        return counters, locs
-
-def get_strong_against(name):
-    with open('champs.json') as json_data:
-        data = json.load(json_data)
-        against = []
-        locs = []
-        for champs in data:
-            if champs['Champion Names'] == name:
-                for col in against_cols:
-                    against.append(parse_name(champs[col]))
-                for col in get_loc_names(against_cols):
-                    locs.append(champs[col])
-        return against, locs
-
-def get_partner(name):
-    with open('champs.json') as json_data:
-        data = json.load(json_data)
-        parters = []
-        for champs in data:
-            if champs['Champion Names'] == name:
-                for col in partner_cols:
-                    parters.append(parse_name(champs[col]))
-        return parters  
-
-def get_tips(name):
-    with open('champs.json') as json_data:
-        data = json.load(json_data)
-        tips = []
-        for champs in data:
-            if champs['Champion Names'] == name:
-                for col in tips_cols:
-                    tips.append(parse_name(champs[col]))    
-        return tips
-
-def format_counter_msg(name, counters, locs):
-    msg = ""
-    for i in range(len(counters)):
-        msg += "{} counters {} at {}. \n".format(counters[i], name, locs[i])
-    return msg
-
-def format_against_msg(name, against, locs):
-    msg = ""
-    for i in range(len(against)):
-        msg += "{} is strong against {} at {}. \n".format(name, against[i], locs[i])
-    return msg
-
-def format_partner_msg(name, partners):
-    msg = ""
-    for i in range(len(partners)):
-        msg += "{} goes well with {}. \n".format(partners[i], name)
-    return msg
-
-def format_tip_msg(name, tips):
-    msg = "To beat {}... \n".format(name)
-    for i in range(len(tips)):
-        msg += "{} \n".format(tips[i])
-    return msg
-
-
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -136,10 +35,12 @@ def callback():
         abort(400)
     return 'OK'
 
+
+
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage("Invalid Champion... Don't play League if you can't type...")
+    message = TextSendMessage(text='Hello, world')
     line_bot_api.reply_message(event.reply_token, message)
     #qryChamp = event.message.text
     #if valid_champ(qryChamp):
