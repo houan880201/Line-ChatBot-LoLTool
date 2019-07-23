12acf9dbd56411c8a84d2b42f000333a4f4b2203
Howard <houan.howard@gmail.com>
2019-07-23 15:03:32 +0800

    try car

--- app.py
+++ app.py
@@ -175,185 +175,24 @@ def handle_message(event):
 		return 0
 
 	if input_str == "yo":
-		line_bot_api.reply_message(event.reply_token,
-		{
-		  "type": "carousel",
-		  "contents": [
-		    {
-		      "type": "bubble",
-		      "hero": {
-		        "type": "image",
-		        "size": "full",
-		        "aspectRatio": "20:13",
-		        "aspectMode": "cover",
-		        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png"
-		      },
-		      "body": {
-		        "type": "box",
-		        "layout": "vertical",
-		        "spacing": "sm",
-		        "contents": [
-		          {
-		            "type": "text",
-		            "text": "Arm Chair, White",
-		            "wrap": True,
-		            "weight": "bold",
-		            "size": "xl"
-		          },
-		          {
-		            "type": "box",
-		            "layout": "baseline",
-		            "contents": [
-		              {
-		                "type": "text",
-		                "text": "$49",
-		                "wrap": True,
-		                "weight": "bold",
-		                "size": "xl",
-		                "flex": 0
-		              },
-		              {
-		                "type": "text",
-		                "text": ".99",
-		                "wrap": True,
-		                "weight": "bold",
-		                "size": "sm",
-		                "flex": 0
-		              }
-		            ]
-		          }
-		        ]
-		      },
-		      "footer": {
-		        "type": "box",
-		        "layout": "vertical",
-		        "spacing": "sm",
-		        "contents": [
-		          {
-		            "type": "button",
-		            "style": "primary",
-		            "action": {
-		              "type": "uri",
-		              "label": "Add to Cart",
-		              "uri": "https://linecorp.com"
-		            }
-		          },
-		          {
-		            "type": "button",
-		            "action": {
-		              "type": "uri",
-		              "label": "Add to wishlist",
-		              "uri": "https://linecorp.com"
-		            }
-		          }
-		        ]
-		      }
-		    },
-		    {
-		      "type": "bubble",
-		      "hero": {
-		        "type": "image",
-		        "size": "full",
-		        "aspectRatio": "20:13",
-		        "aspectMode": "cover",
-		        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_6_carousel.png"
-		      },
-		      "body": {
-		        "type": "box",
-		        "layout": "vertical",
-		        "spacing": "sm",
-		        "contents": [
-		          {
-		            "type": "text",
-		            "text": "Metal Desk Lamp",
-		            "wrap": True,
-		            "weight": "bold",
-		            "size": "xl"
-		          },
-		          {
-		            "type": "box",
-		            "layout": "baseline",
-		            "flex": 1,
-		            "contents": [
-		              {
-		                "type": "text",
-		                "text": "$11",
-		                "wrap": True,
-		                "weight": "bold",
-		                "size": "xl",
-		                "flex": 0
-		              },
-		              {
-		                "type": "text",
-		                "text": ".99",
-		                "wrap": True,
-		                "weight": "bold",
-		                "size": "sm",
-		                "flex": 0
-		              }
-		            ]
-		          },
-		          {
-		            "type": "text",
-		            "text": "Temporarily out of stock",
-		            "wrap": True,
-		            "size": "xxs",
-		            "margin": "md",
-		            "color": "#ff5551",
-		            "flex": 0
-		          }
-		        ]
-		      },
-		      "footer": {
-		        "type": "box",
-		        "layout": "vertical",
-		        "spacing": "sm",
-		        "contents": [
-		          {
-		            "type": "button",
-		            "flex": 2,
-		            "style": "primary",
-		            "color": "#aaaaaa",
-		            "action": {
-		              "type": "uri",
-		              "label": "Add to Cart",
-		              "uri": "https://linecorp.com"
-		            }
-		          },
-		          {
-		            "type": "button",
-		            "action": {
-		              "type": "uri",
-		              "label": "Add to wish list",
-		              "uri": "https://linecorp.com"
-		            }
-		          }
-		        ]
-		      }
-		    },
-		    {
-		      "type": "bubble",
-		      "body": {
-		        "type": "box",
-		        "layout": "vertical",
-		        "spacing": "sm",
-		        "contents": [
-		          {
-		            "type": "button",
-		            "flex": 1,
-		            "gravity": "center",
-		            "action": {
-		              "type": "uri",
-		              "label": "See more",
-		              "uri": "https://linecorp.com"
-		            }
-		          }
-		        ]
-		      }
-		    }
-		  ]
-		})
-		return 0
+		carousel_template_message = TemplateSendMessage(
+            alt_text='ImageCarousel template',
+            template=ImageCarouselTemplate(
+                columns=[
+                    ImageCarouselColumn(
+                        image_url='https://i.imgur.com/g8zAYMq.jpg',
+                        action=URIAction(
+                            label='加我好友試玩',
+                            uri='https://line.me/R/ti/p/%40gmy1077x'
+                        ),
+                    ),
+                ]
+            )
+        )
+        line_bot_api.reply_message(
+            event.reply_token,
+            carousel_template_message)
+        return 0
 
 	splited = input_str.split(' ')
 
